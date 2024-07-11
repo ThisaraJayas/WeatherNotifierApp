1e5e3b38ec8dbe014391613751ff2c060c6fb4f2
Thisara <sasmithajayasinghe1@gmail.com>
2024-07-11 22:42:01 +0530

    user controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,1 +1,11 @@
-d
+import User from '../models/UserModel'
+export const saveUser = async(req,res)=>{
+    const { email, location } = req.body;
+    try{
+        const user = new User({email,location})
+        await user.save()
+        res.status(200).json({message:"User Created Success",user})
+    }catch(error){
+        res.status(400).json({message:"User Not Created",error})
+    }
+}

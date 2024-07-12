6686580ffea436ec5dcbbd6a119aee94a1e52dcf
Thisara <sasmithajayasinghe1@gmail.com>
2024-07-12 23:16:48 +0530

    ai

--- utils/generateWeatherDescription .js
+++ utils/generateWeatherDescription .js
@@ -3,6 +3,7 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 const genAI = new GoogleGenerativeAI("AIzaSyB7ecIK4Q3aw3RLm5jHdmsuj40lsaxVTmw");
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 console.log('API Key:', process.env.API_KEY);
+
 export const generateWeatherDescription = async (weatherData) => {
     const prompt = `Generate a detailed weather report based on the following data and format text beautifully and long dont put * sign: 
     Temperature: ${(weatherData.main.temp - 273.15).toFixed(0)} °C, 

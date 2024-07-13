ac493fa35f6467ebc56dd39942fff9b3f0838e2e
Thisara <sasmithajayasinghe1@gmail.com>
2024-07-13 15:12:04 +0530

    url path update

---
+++ url.js
@@ -0,0 +1,2 @@
+export const WEATHER_URL="http://api.openweathermap.org/geo/1.0/"
+export const GET_WEATHERDATA_URL="https://api.openweathermap.org/data/2.5/weather"
--- utils/WeatherUtils.js
+++ utils/WeatherUtils.js
@@ -1,10 +1,11 @@
 import axios from "axios"
 import { getLocationCoordinates } from "./LocationCordinateUtils.js"
+import { GET_WEATHERDATA_URL } from "../url.js"
 
 export const getWeatherData = async(cityName,countryCode)=>{
     try{
         const {lat,lon} = await getLocationCoordinates(cityName,countryCode)
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`)
+        const response = await axios.get(`${GET_WEATHERDATA_URL}?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`)
         return {data: response.data}
     }catch(error){
         return {error}
